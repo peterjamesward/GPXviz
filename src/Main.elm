686123a68899e399657238365a711b57bb8b895e
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-11-09 17:07:25 +0000

    regex is promising for quick parser.

--- src/Main.elm
+++ src/Main.elm
@@ -3,9 +3,10 @@ module Main exposing (main)
 import Browser
 import File exposing (File)
 import File.Select as Select
-import Html exposing (Html, button, p, text)
+import Html exposing (Html, button, div, p, text)
 import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Regex
 import Task
 
 
@@ -15,26 +16,38 @@ import Task
 
 main : Program () Model Msg
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
+type alias TrackPoint =
+    { lat : String
+    , lon : String
+    , ele : String
+    }
+
+
 type alias Model =
-  { gpx : Maybe String
-  }
+    { gpx : Maybe String
+    , trackPoints : List TrackPoint
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model Nothing, Cmd.none )
+    ( { gpx = Nothing
+      , trackPoints = []
+      }
+    , Cmd.none
+    )
 
 
 
@@ -42,28 +55,65 @@ init _ =
 
 
 type Msg
-  = GpxRequested
-  | GpxSelected File
-  | GpxLoaded String
+    = GpxRequested
+    | GpxSelected File
+    | GpxLoaded String
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    GpxRequested ->
-      ( model
-      , Select.file ["text/gpx"] GpxSelected
-      )
+    case msg of
+        GpxRequested ->
+            ( model
+            , Select.file [ "text/gpx" ] GpxSelected
+            )
 
-    GpxSelected file ->
-      ( model
-      , Task.perform GpxLoaded (File.toString file)
-      )
+        GpxSelected file ->
+            ( model
+            , Task.perform GpxLoaded (File.toString file)
+            )
 
-    GpxLoaded content ->
-      ( { model | gpx = Just content }
-      , Cmd.none
-      )
+        GpxLoaded content ->
+            ( { model
+                | gpx = Just content
+                , trackPoints = parseTrackPoints content
+              }
+            , Cmd.none
+            )
+
+
+parseTrackPoints : String -> List TrackPoint
+parseTrackPoints xml =
+    let
+        reg t =
+            Maybe.withDefault Regex.never <| Regex.fromString t
+
+        latitudes =
+            Regex.find (reg "lat=\\\"[\\d\\.]*\\\"") xml |> matches
+
+        longitudes =
+            Regex.find (reg "lon=\\\"[\\d\\.]*\\\"") xml |> matches
+
+        elevations =
+            Regex.find (reg "<ele>[\\d\\.]*</ele>") xml |> matches
+
+        makeTrackPoint a b c =
+            { lat = a
+            , lon = b
+            , ele = c
+            }
+
+        matches xs =
+            List.map value xs
+
+        value x =
+            x.match
+    in
+    List.map3
+        makeTrackPoint
+        latitudes
+        longitudes
+        elevations
 
 
 
@@ -72,12 +122,18 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  case model.gpx of
-    Nothing ->
-      button [ onClick GpxRequested ] [ text "Load GPX" ]
+    case model.gpx of
+        Nothing ->
+            button [ onClick GpxRequested ] [ text "Load GPX" ]
 
-    Just content ->
-      p [ style "white-space" "pre" ] [ text content ]
+        Just _ ->
+            div [] <|
+                List.map viewTrackPoint model.trackPoints
+
+
+viewTrackPoint : TrackPoint -> Html Msg
+viewTrackPoint trkpnt =
+    p [ style "white-space" "pre" ] [ text trkpnt.lat ]
 
 
 
@@ -86,4 +142,4 @@ view model =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
