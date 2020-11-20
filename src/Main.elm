b246c9b1500445751125901189bf1d7295ac3dc0
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-11-20 11:21:35 +0000

    About text update.

--- src/Main.elm
+++ src/Main.elm
@@ -1869,17 +1869,18 @@ bendSmoothnessSlider model =
 aboutText =
     """Thank you for trying this GPX viewer. It is freely provided without warranty.
 
-**Overview** shows a route overview and summary statistics. Zoom is (currently) fixed on the centre of the area and there is no pan capability.
+**Overview** shows a route overview and summary statistics. Zoom is (currently) fixed on the centre of the area and there is no pan capability. You can change the presentation style using the options on the right.
 
 **First person** positions the viewpoint above a track _road segment_, sighted along the track. The zoom control will move your viewpoint back and forth a bit. The bottom slider and arrows move between track segments. Information about the current segment is shown.
 
-**Third person** focuses on track _points_ and lets you fly around the current point.  An orange disc will indicate the position on the track. The slider and arrows move to other track points. Information about the current track point is shown. **NEW** You can apply fixes in this view using the "Fixes" section on the right hand side.
+**Third person** focuses on track _points_ and lets you fly around the current point.  An orange disc will indicate the position on the track. The slider and arrows move to other track points. Information about the current track point is shown. On the right hand side, you can click "Gradient smoother" to smooth dodgy gradients.
 
-**Problems** details issues with the GPX that you may want to "fix".
+> **Gradient smoother** will show (at the bottom right) a list of track points where the gradient suddenly changes by more than the threshold (you set this with the slider). You may click on this list to move directly to that place in the view.
+To smooth a section of road, "Drop marker" places a purple cone on the track; then move your current position (backwards or forwards) so that you 'bracket' a section of road. The big button will then let you 'smooth' this part of the road. You choose how bumpy you would like it: 0 is totally smooth, 1 is basically unchanged. You can experiment, and easily "Undo".
 
-**Options** (this page) lets you turn off some elements. You may like this depending on your motivation; it may help to turn some things off with a complex track. _Gradient change threshold_ will cause markers to hover over any track points where the gradient change exceed this value.
+**About** -- that's this message.
 
-Simply click the blue button at the page top to choose a file.
+Click the blue button at the page top to choose a file.
 
 > _Peter Ward, 2020_
 """
