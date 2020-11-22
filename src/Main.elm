c94e3002fc28d89f52e0f96b73eef89e4d792480
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-11-22 12:09:06 +0000

    Moving functions into their new homes.

--- src/Main.elm
+++ src/Main.elm
@@ -25,7 +25,7 @@ import Length exposing (meters)
 import List exposing (drop, tail, take)
 import Markdown exposing (defaultOptions)
 import Msg exposing (..)
-import NodesAndRoads exposing (DrawingNode, DrawingRoad, MyCoord)
+import NodesAndRoads exposing (DrawingNode, DrawingRoad, MyCoord, SummaryData, deriveNodes, deriveRoads, deriveSummary)
 import Pixels exposing (Pixels)
 import Point3d
 import Scene3d exposing (Entity, cone, cylinder)
@@ -128,17 +128,6 @@ type alias Flythrough =
     , lastUpdated : Time.Posix
     , running : Bool
     , segment : DrawingRoad
-    }
-
-
-type alias SummaryData =
-    { highestMetres : Float
-    , lowestMetres : Float
-    , trackLength : Float
-    , climbingDistance : Float
-    , descendingDistance : Float
-    , totalClimbing : Float
-    , totalDescending : Float
     }
 
 
@@ -938,135 +927,6 @@ parseGPXintoModel content model =
             else
                 model.viewingMode
     }
-
-
-deriveNodes : ScalingInfo -> List TrackPoint -> List DrawingNode
-deriveNodes scale tps =
-    let
-        elevationToClipSpace e =
-            (e - findCentres.ele) * scale.metresToClipSpace
-
-        findCentres =
-            scale.centres
-
-        mins =
-            scale.mins
-
-        prepareDrawingNode tp =
-            { trackPoint = tp
-            , northOffset = (tp.lat - mins.lat) * metresPerDegreeLatitude
-            , eastOffset = (tp.lon - mins.lon) * metresPerDegreeLatitude * cos tp.lat
-            , vertOffset = tp.ele - mins.ele
-            , x = (tp.lon - findCentres.lon) / (0.5 * scale.largestDimension)
-            , y = (tp.lat - findCentres.lat) / (0.5 * scale.largestDimension)
-            , z = elevationToClipSpace tp.ele
-            }
-    in
-    List.map prepareDrawingNode tps
-
-
-deriveRoads : List DrawingNode -> List DrawingRoad
-deriveRoads drawingNodes =
-    let
-        roadSegment node1 node2 =
-            let
-                zDifference =
-                    node2.trackPoint.ele - node1.trackPoint.ele
-
-                earthDistance =
-                    -- Great circle distance (!) ignoring elevation difference
-                    Spherical.range ( degrees node2.trackPoint.lat, degrees node2.trackPoint.lon )
-                        ( degrees node1.trackPoint.lat, degrees node1.trackPoint.lon )
-            in
-            { startsAt = node1
-            , endsAt = node2
-            , length = earthDistance
-            , bearing =
-                Spherical.findBearingToTarget
-                    ( degrees node1.trackPoint.lat, degrees node1.trackPoint.lon )
-                    ( degrees node2.trackPoint.lat, degrees node2.trackPoint.lon )
-            , gradient =
-                if earthDistance > 0 then
-                    100.0 * (zDifference / earthDistance)
-
-                else
-                    0.0
-            , startDistance = 0.0
-            , endDistance = 0.0
-            , index = 0
-            }
-
-        roadSegments =
-            List.map2 roadSegment
-                drawingNodes
-                (List.drop 1 drawingNodes)
-
-        ( _, _, withAccumulations ) =
-            List.foldl
-                (\road ( idx, dist, done ) ->
-                    ( idx + 1
-                    , dist + road.length
-                    , { road
-                        | startDistance = dist
-                        , endDistance = dist + road.length
-                        , index = idx
-                      }
-                        :: done
-                    )
-                )
-                ( 0, 0.0, [] )
-                roadSegments
-    in
-    List.reverse withAccumulations
-
-
-deriveSummary : List DrawingRoad -> SummaryData
-deriveSummary roadSegments =
-    let
-        accumulateInfo segment summary =
-            { trackLength = summary.trackLength + segment.length
-            , highestMetres =
-                max summary.highestMetres <|
-                    max segment.startsAt.trackPoint.ele segment.endsAt.trackPoint.ele
-            , lowestMetres =
-                min summary.lowestMetres <|
-                    min segment.startsAt.trackPoint.ele segment.endsAt.trackPoint.ele
-            , climbingDistance =
-                if segment.gradient > 0 then
-                    summary.climbingDistance + segment.length
-
-                else
-                    summary.climbingDistance
-            , descendingDistance =
-                if segment.gradient < 0 then
-                    summary.climbingDistance + segment.length
-
-                else
-                    summary.climbingDistance
-            , totalClimbing =
-                if segment.gradient > 0 then
-                    summary.totalClimbing + segment.endsAt.trackPoint.ele - segment.startsAt.trackPoint.ele
-
-                else
-                    summary.totalClimbing
-            , totalDescending =
-                if segment.gradient < 0 then
-                    summary.totalClimbing - segment.endsAt.trackPoint.ele + segment.startsAt.trackPoint.ele
-
-                else
-                    summary.totalClimbing
-            }
-    in
-    List.foldl accumulateInfo
-        { trackLength = 0.0
-        , highestMetres = -9999.9
-        , lowestMetres = 9999.9
-        , climbingDistance = 0.0
-        , descendingDistance = 0.0
-        , totalClimbing = 0.0
-        , totalDescending = 0.0
-        }
-        roadSegments
 
 
 deriveNodesAndRoads : Model -> Model
--- src/NodesAndRoads.elm
+++ src/NodesAndRoads.elm
@@ -1,6 +1,7 @@
 module NodesAndRoads exposing (..)
 
-import TrackPoint exposing (TrackPoint)
+import Spherical exposing (metresPerDegreeLatitude)
+import TrackPoint exposing (ScalingInfo, TrackPoint)
 
 
 type MyCoord
@@ -29,3 +30,143 @@ type alias DrawingRoad =
     , endDistance : Float
     , index : Int
     }
+
+
+type alias SummaryData =
+    { highestMetres : Float
+    , lowestMetres : Float
+    , trackLength : Float
+    , climbingDistance : Float
+    , descendingDistance : Float
+    , totalClimbing : Float
+    , totalDescending : Float
+    }
+
+
+deriveNodes : ScalingInfo -> List TrackPoint -> List DrawingNode
+deriveNodes scale tps =
+    let
+        elevationToClipSpace e =
+            (e - findCentres.ele) * scale.metresToClipSpace
+
+        findCentres =
+            scale.centres
+
+        mins =
+            scale.mins
+
+        prepareDrawingNode tp =
+            { trackPoint = tp
+            , northOffset = (tp.lat - mins.lat) * metresPerDegreeLatitude
+            , eastOffset = (tp.lon - mins.lon) * metresPerDegreeLatitude * cos tp.lat
+            , vertOffset = tp.ele - mins.ele
+            , x = (tp.lon - findCentres.lon) / (0.5 * scale.largestDimension)
+            , y = (tp.lat - findCentres.lat) / (0.5 * scale.largestDimension)
+            , z = elevationToClipSpace tp.ele
+            }
+    in
+    List.map prepareDrawingNode tps
+
+
+deriveRoads : List DrawingNode -> List DrawingRoad
+deriveRoads drawingNodes =
+    let
+        roadSegment node1 node2 =
+            let
+                zDifference =
+                    node2.trackPoint.ele - node1.trackPoint.ele
+
+                earthDistance =
+                    -- Great circle distance (!) ignoring elevation difference
+                    Spherical.range ( degrees node2.trackPoint.lat, degrees node2.trackPoint.lon )
+                        ( degrees node1.trackPoint.lat, degrees node1.trackPoint.lon )
+            in
+            { startsAt = node1
+            , endsAt = node2
+            , length = earthDistance
+            , bearing =
+                Spherical.findBearingToTarget
+                    ( degrees node1.trackPoint.lat, degrees node1.trackPoint.lon )
+                    ( degrees node2.trackPoint.lat, degrees node2.trackPoint.lon )
+            , gradient =
+                if earthDistance > 0 then
+                    100.0 * (zDifference / earthDistance)
+
+                else
+                    0.0
+            , startDistance = 0.0
+            , endDistance = 0.0
+            , index = 0
+            }
+
+        roadSegments =
+            List.map2 roadSegment
+                drawingNodes
+                (List.drop 1 drawingNodes)
+
+        ( _, _, withAccumulations ) =
+            List.foldl
+                (\road ( idx, dist, done ) ->
+                    ( idx + 1
+                    , dist + road.length
+                    , { road
+                        | startDistance = dist
+                        , endDistance = dist + road.length
+                        , index = idx
+                      }
+                        :: done
+                    )
+                )
+                ( 0, 0.0, [] )
+                roadSegments
+    in
+    List.reverse withAccumulations
+
+
+deriveSummary : List DrawingRoad -> SummaryData
+deriveSummary roadSegments =
+    let
+        accumulateInfo segment summary =
+            { trackLength = summary.trackLength + segment.length
+            , highestMetres =
+                max summary.highestMetres <|
+                    max segment.startsAt.trackPoint.ele segment.endsAt.trackPoint.ele
+            , lowestMetres =
+                min summary.lowestMetres <|
+                    min segment.startsAt.trackPoint.ele segment.endsAt.trackPoint.ele
+            , climbingDistance =
+                if segment.gradient > 0 then
+                    summary.climbingDistance + segment.length
+
+                else
+                    summary.climbingDistance
+            , descendingDistance =
+                if segment.gradient < 0 then
+                    summary.climbingDistance + segment.length
+
+                else
+                    summary.climbingDistance
+            , totalClimbing =
+                if segment.gradient > 0 then
+                    summary.totalClimbing + segment.endsAt.trackPoint.ele - segment.startsAt.trackPoint.ele
+
+                else
+                    summary.totalClimbing
+            , totalDescending =
+                if segment.gradient < 0 then
+                    summary.totalClimbing - segment.endsAt.trackPoint.ele + segment.startsAt.trackPoint.ele
+
+                else
+                    summary.totalClimbing
+            }
+    in
+    List.foldl accumulateInfo
+        { trackLength = 0.0
+        , highestMetres = -9999.9
+        , lowestMetres = 9999.9
+        , climbingDistance = 0.0
+        , descendingDistance = 0.0
+        , totalClimbing = 0.0
+        , totalDescending = 0.0
+        }
+        roadSegments
