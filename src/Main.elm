ef2853d4eccc52f183721bcd5ca650c6fbdc3667
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-11-22 08:51:47 +0000

    ToDos get their own file.

--- src/Main.elm
+++ src/Main.elm
@@ -43,14 +43,6 @@ import WriteGPX exposing (writeGPX)
 
 
 
---TODO: Linear orthographic ride profile.
---TODO: Break up this module.
---TODO: Try chamfer for horizontal turns.
---TODO: Autofix bends with circular arcs.
---TODO: Improve removal of zero lengths so we don't have to repeat ourselves!
---TODO: Allow drag & zoom in 1st person including flythrough (offset, not angles).
---TODO: Some way to join the start and end of a loop (although MR does this).
---TODO: Remove duplicate helpers.
 
 
 main : Program () Model Msg
---
+++ src/ToDo.elm
@@ -0,0 +1,11 @@
+module ToDo exposing (..)
+
+--TODO: Save static display artefacts separately and combine them when needed.
+--TODO: Linear orthographic ride profile.
+--TODO: Break up this module.
+--TODO: Try chamfer for horizontal turns.
+--TODO: Autofix bends with circular arcs.
+--TODO: Improve removal of zero lengths so we don't have to repeat ourselves!
+--TODO: Allow drag & zoom in 1st person including flythrough (offset, not angles).
+--TODO: Some way to join the start and end of a loop (although MR does this).
+--TODO: Remove duplicate helpers.
