4e276a6ecc56a8613446d5310441aaf9a68fdb64
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2020-11-28 13:13:50 +0000

    Imperfect but encouraging terrain.

--- src/Terrain.elm
+++ src/Terrain.elm
@@ -4,12 +4,14 @@ import Array exposing (Array)
 import Color
 import DelaunayTriangulation2d exposing (DelaunayTriangulation2d, Error, faces, fromVerticesBy)
 import Length exposing (Meters)
+import List exposing (take)
 import NodesAndRoads exposing (DrawingRoad, MyCoord, deriveNodes, deriveRoads)
 import Point2d exposing (Point2d)
-import Point3d
+import Point3d exposing (Point3d)
 import RenderingContext exposing (RenderingContext)
 import Scene3d exposing (Entity)
 import Scene3d.Material as Material
+import SketchPlane3d
 import Spherical exposing (metresPerDegreeLatitude)
 import Triangle3d
 
@@ -18,10 +20,11 @@ import Triangle3d
 {-
    The idea here is to make a mesh using Delaunay Triangulation,
    included in the elm-geometry package.
-   There is one complications remaining:
+   There are two complications remaining:
    1.  We shall need to work from the road edges rather than centres, so
        we will need the road segment corner coordinates. Ideally we would
        compute these once.
+   2.  Probably needs some variation in colour.
 -}
 
 
@@ -31,29 +34,70 @@ makeTerrain :
     -> List (Entity MyCoord)
 makeTerrain context roads =
     let
-        pointFromRoad : DrawingRoad -> Point2d Meters MyCoord
-        pointFromRoad road =
-            Point2d.meters road.startsAt.x road.startsAt.y
+        convertTo2d p3d =
+            let
+                ( x, y, _ ) =
+                    Point3d.toTuple Length.inMeters p3d
+            in
+            Point2d.meters x y
+
+        roadList =
+            Array.toList roads ++ theEdgesOfOurWorld
+
+        roadVertices =
+            List.concatMap entryEdges (List.take 1 roadList)
+                ++ List.concatMap exitEdges roadList
 
         maybeTriangulation =
             -- This looks like the perfect constructor for us, if
             -- we ignore that we ignore the last trackpoint. (Experiment, right?)
             fromVerticesBy
-                pointFromRoad
-                withCorners
+                (Point3d.projectInto SketchPlane3d.xy)
+            <|
+                Array.fromList roadVertices
 
         corner lat lon ele =
-            -- Make a trackpoint to locate each of our corners.
+            -- Make a trackpoint to locate each of our corners of the world.
             { lat = lat
             , lon = lon
             , ele = ele
             , idx = 0
             }
 
+        entryEdges road =
+            List.take 2 <| roadCorners road
+
+        exitEdges road =
+            List.drop 2 <| roadCorners road
+
+        roadCorners road =
+            -- Duplicated from VisualEntities with no apology.
+            let
+                kerbX =
+                    -- Road is assumed to be 6 m wide.
+                    4.0 * cos road.bearing * context.scaling.metresToClipSpace
+
+                kerbY =
+                    4.0 * sin road.bearing * context.scaling.metresToClipSpace
+
+                thickness =
+                    0.6 * context.scaling.metresToClipSpace
+
+                ( ( x1, y1, z1 ), ( x2, y2, z2 ) ) =
+                    ( ( road.startsAt.x, road.startsAt.y, road.startsAt.z )
+                    , ( road.endsAt.x, road.endsAt.y, road.endsAt.z )
+                    )
+            in
+            [ Point3d.meters (x1 + kerbX) (y1 - kerbY) (z1 - thickness)
+            , Point3d.meters (x2 + kerbX) (y2 - kerbY) (z2 - thickness)
+            , Point3d.meters (x2 - kerbX) (y2 + kerbY) (z2 - thickness)
+            , Point3d.meters (x1 - kerbX) (y1 + kerbY) (z1 - thickness)
+            ]
+
         boundary =
             1000.0 / metresPerDegreeLatitude
 
-        cornerTrackpoints =
+        externalTrackpoints =
             -- Not pretty, but heh.
             -- NB we return to the first corner because we're only using road starts!
             [ corner (context.scaling.mins.lat - boundary)
@@ -73,12 +117,9 @@ makeTerrain context roads =
                 context.scaling.mins.ele
             ]
 
-        cornerRoads =
-            deriveNodes context.scaling cornerTrackpoints
+        theEdgesOfOurWorld =
+            deriveNodes context.scaling externalTrackpoints
                 |> deriveRoads
-
-        withCorners =
-            Array.append roads <| Array.fromList cornerRoads
     in
     case maybeTriangulation of
         Err _ ->
@@ -95,9 +136,6 @@ makeTerrain context roads =
                             face.vertices
                     in
                     Scene3d.triangle (Material.color Color.darkGreen) <|
-                        Triangle3d.from
-                            (Point3d.meters v1.startsAt.x v1.startsAt.y v1.startsAt.z)
-                            (Point3d.meters v2.startsAt.x v2.startsAt.y v2.startsAt.z)
-                            (Point3d.meters v3.startsAt.x v3.startsAt.y v3.startsAt.z)
+                        Triangle3d.from v1 v2 v3
             in
             List.map drawFace (faces triangulation)
