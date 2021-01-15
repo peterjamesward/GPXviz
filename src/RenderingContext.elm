7243257a921959de82766548e59ec85c3fd51310
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-01-15 03:01:46 +0000

    Bend smoother looks OK.

--- src/RenderingContext.elm
+++ src/RenderingContext.elm
@@ -6,7 +6,6 @@ import Length
 import NodesAndRoads exposing (DrawingNode, DrawingRoad, LocalCoords, ScalingInfo)
 import Point3d exposing (Point3d)
 import ViewTypes exposing (ViewingMode)
-import BendSmoother exposing (SmoothedBend)
 
 
 type alias RenderingContext =
