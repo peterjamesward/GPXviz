f7dcb3e7a34dad0225a8313feb3c94fedb085fb0
Pete Ward <34382358+peterjamesward@users.noreply.github.com>
2021-03-15 09:42:34 +0000

    Before rewriting node detection.

--- src/ToDo.elm
+++ src/ToDo.elm
@@ -8,7 +8,21 @@ module ToDo exposing (..)
 
 --TODO: John Bytheway suggestion.
 
---TODO: Remove false nodes by checking the direction of neighbours, not just the count.
+--TODO: Avoid false nodes by checking the direction of neighbours, not just the count.
+This is a fairly big change, albeit limited to within Graph.
+Changes the definition of interestingTrackPoint to one that has neighbours in more than two directions.
+Changes the definition of edge to one that goes from one node to another, leaving in a direction.
+
+Rewrite neighbour counting.
+Can't use Dict because of possible small errors in bearings.
+Hence, build list of bearings to each neighbour.
+Ignore small errors (say, 1 degree).
+Track point is node if more than two directions in final list.
+
+Canonical edge detection essentially the same (first passage is taken as canonical).
+Edge lookup also cannot use Dict because of minor bearing inaccuracies.
+Two A-B edges are distinguished by the outgoing bearing at A, and these live in an association list.
+
 --THEN: Use Graph to check whether edits allowed.
 --THEN: Work through all the editing functions to work on the graph ("Frozen" mode).
 --THEN: Route DSL.
